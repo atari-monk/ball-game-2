a98f536f85e4cf7cecb68e0c44c85628e625c4d4
atari-monk <atari.monk1@gmail.com>
2023-10-07 16:12:37 +0200

    wall collision fix

--- game/src/collision/PlayerWallCollider.ts
+++ game/src/collision/PlayerWallCollider.ts
@@ -9,58 +9,22 @@ export class PlayerWallCollider {
     for (const player of players) {
       const newX = player.x + player.velocityX * deltaTime
       const newY = player.y + player.velocityY * deltaTime
-      let directionChanged = false
 
-      if (newX - player.radius < 0 || newX + player.radius > field.width) {
-        this.adjustHorizontalWallCollision(player, field, newX)
-        directionChanged = true
+      if (newX - player.radius < 0) {
+        player.x = player.radius
+      } else if (newX + player.radius > field.width) {
+        player.x = field.width - player.radius
+      } else {
+        player.x = newX
       }
 
-      if (newY - player.radius < 0 || newY + player.radius > field.height) {
-        this.adjustVerticalWallCollision(player, field, newY)
-        directionChanged = true
+      if (newY - player.radius < 0) {
+        player.y = player.radius
+      } else if (newY + player.radius > field.height) {
+        player.y = field.height - player.radius
+      } else {
+        player.y = newY
       }
-
-      if (directionChanged) {
-        this.normalizePlayerDirection(player)
-      }
     }
-  }
-
-  private adjustHorizontalWallCollision(
-    player: IPlayer,
-    field: IField,
-    newX: number
-  ) {
-    if (newX - player.radius < 0) {
-      player.x = player.radius
-    } else {
-      player.x = field.width - player.radius
-    }
-
-    player.direction = Math.PI - player.direction
-  }
-
-  private adjustVerticalWallCollision(
-    player: IPlayer,
-    field: IField,
-    newY: number
-  ) {
-    if (newY - player.radius < 0) {
-      player.y = player.radius
-    } else {
-      player.y = field.height - player.radius
-    }
-
-    player.direction = -player.direction
-  }
-
-  private normalizePlayerDirection(player: IPlayer) {
-    const length = Math.sqrt(
-      player.velocityX * player.velocityX + player.velocityY * player.velocityY
-    )
-
-    player.velocityX /= length
-    player.velocityY /= length
   }
 }
---
+++ game/src/collision/PlayerWallCollider_Deprecated.ts
@@ -0,0 +1,66 @@
+import { IField, IPlayer } from 'game-api'
+
+export class PlayerWallCollider_Deprecated {
+  public checkWallCollision(
+    players: IPlayer[],
+    field: IField,
+    deltaTime: number
+  ) {
+    for (const player of players) {
+      const newX = player.x + player.velocityX * deltaTime
+      const newY = player.y + player.velocityY * deltaTime
+      let directionChanged = false
+
+      if (newX - player.radius < 0 || newX + player.radius > field.width) {
+        this.adjustHorizontalWallCollision(player, field, newX)
+        directionChanged = true
+      }
+
+      if (newY - player.radius < 0 || newY + player.radius > field.height) {
+        this.adjustVerticalWallCollision(player, field, newY)
+        directionChanged = true
+      }
+
+      if (directionChanged) {
+        this.normalizePlayerDirection(player)
+      }
+    }
+  }
+
+  private adjustHorizontalWallCollision(
+    player: IPlayer,
+    field: IField,
+    newX: number
+  ) {
+    if (newX - player.radius < 0) {
+      player.x = player.radius
+    } else {
+      player.x = field.width - player.radius
+    }
+
+    player.direction = Math.PI - player.direction
+  }
+
+  private adjustVerticalWallCollision(
+    player: IPlayer,
+    field: IField,
+    newY: number
+  ) {
+    if (newY - player.radius < 0) {
+      player.y = player.radius
+    } else {
+      player.y = field.height - player.radius
+    }
+
+    player.direction = -player.direction
+  }
+
+  private normalizePlayerDirection(player: IPlayer) {
+    const length = Math.sqrt(
+      player.velocityX * player.velocityX + player.velocityY * player.velocityY
+    )
+
+    player.velocityX /= length
+    player.velocityY /= length
+  }
+}
