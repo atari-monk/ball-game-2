28d70eaab3e3370527911e0ca32bf824dac41428
atari-monk <atari.monk1@gmail.com>
2023-10-20 18:43:43 +0200

    dom-lib

---
+++ dom-lib/script/i-lib.ps1
@@ -0,0 +1,10 @@
+. "C:\atari-monk\Code\ball-game-2\ps1\CommonFunctions.ps1"
+
+$libName = "dom-lib"
+$build = $libName + "\build\"
+$pack = $libName + "-1.0.0.tgz"
+$lib = $RepoPath + $build + $pack
+$targetProj1 = $RepoPath + "client\"
+
+Build-Lib
+Copy-And-Install-Pack -packPath $lib -projDir $targetProj1 -packName $pack
