362e8ce83943bf17925dd86b0444488d1dc1ebdf
atari-monk <atari.monk1@gmail.com>
2023-10-20 19:08:24 +0200

    scripts

---
+++ js_script/copy-package.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const path = require('path')
+
+function copyFileToBuild(sourceFile, targetDir) {
+  const scriptDir = __dirname
+  const projectRootDir = path.resolve(scriptDir, '.')
+  console.log('projectRootDir:', projectRootDir)
+  const sourcePath = path.join(projectRootDir, sourceFile)
+  const targetPath = path.join(
+    projectRootDir,
+    targetDir,
+    path.basename(sourceFile)
+  )
+
+  if (!fs.existsSync(path.dirname(targetPath))) {
+    fs.mkdirSync(path.dirname(targetPath), { recursive: true })
+  }
+
+  fs.copyFileSync(sourcePath, targetPath)
+
+  console.log(`Copied ${sourcePath} to ${targetPath}`)
+}
+
+module.exports = copyFileToBuild
